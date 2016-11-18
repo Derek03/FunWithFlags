e42e7f42f7d565e9290abe19a16e6db025822bba
Derek Larson <a00925820@btechgames-imac-16.local>
2016-11-18 11:53:00 -0800

    Initial Commit

---
+++ FunWithFlags/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunWithFlags
+//
+//  Created by Derek Larson on 2016-11-18.
+//  Copyright © 2016 Derek Larson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
